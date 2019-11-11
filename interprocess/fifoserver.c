0c5459a13ff4de163c094f4472b8fc86133ef4e1
Raghavendra <53818125+Ragzbiradar@users.noreply.github.com>
2019-11-11 15:28:15 +0530

    Update fifoserver.c

--- interprocess/fifoserver.c
+++ interprocess/fifoserver.c
@@ -8,21 +8,31 @@
 #define PERMS 0666
 char fname[256];
 int main(){
-    int readfd,writefd;
+    int readfd,writefd,fd;
     ssize_t n;
     char buff[512];
-    printf("trying to connect server....\n");
-    writefd=open(FIFO1,O_WRONLY,0);
-    readfd=open(FIFO2,O_RDONLY,0);
-    printf("connected...\n");
-    printf("enter the filename to request from server:");
-    scanf("%s",fname);
-    write(writefd,fname,strlen(fname));
-    printf("waiting for server to replay...\n ");
-    while((n=read(readfd,buff,512))>0){
-        write(1,buff,n);
+    if(mkfifo(FIFO1,PERMS)<0){
+        printf("cant create fifo1 file \n");
     }
-    close(readfd);
-    close(writefd);
+     if(mkfifo(FIFO2,PERMS)<0){
+        printf("cant create fifo2  file \n");
+    }
+    printf("waiting for connection request....\n");
+    readfd=open(FIFO1,O_RDONLY,0);
+    writefd=open(FIFO2,O_WRONLY,0);
+    printf("connection Established...\n");
+    read(readfd,fname,255);
+    printf("client has requested file %s\n",fname);
+    if((fd=open(fname,O_RDWR))<0){
+        strcpy(buff,"File doesnot exist...\n");
+        write(writefd,buff,strlen(buff));
+    }
+    else{
+        while((n=read(fd,buff,512))>0){
+            write(writefd,buff,n);
+        }
+    }
+    close(readfd);unlink(FIFO1);
+    close(writefd);unlink(FIFO2);
     return 0;
 }
